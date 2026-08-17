fa2a101a2e8f0f9938c1ef9bd8d382ceb366e69f
mcabete <00001147573736sp@al.educacao.sp.gov.br>
2026-08-17 09:39:22 -0300

    Inicio do js versão 1
    Co-authored-by: Yuri Mauricio <KkN0body@users.noreply.github.com>
    Co-authored-by: Gabriel Souza <gabrielrodrigues190@users.noreply.github.com>

--- JAVASCRIPT/principal.js
+++ JAVASCRIPT/principal.js
@@ -1,1 +1,65 @@
-sasasasdasdasdlet 
+let tarefas = [];
+
+function cadastrarMat() {
+
+    let input_textMatId = document.getElementById('input_textMatId').value
+
+    if (input_textMatId !== '') {
+
+        tarefas.push({
+
+            nome: input_textMatId
+
+        });
+
+        mostrar();
+
+    } else {
+
+        alert('Preencha os campos')
+
+    }
+
+    limparInputs();
+
+}
+
+function mostrar() {
+    let div_tableMatResId = document.getElementById('div_tableMatResId');
+    let conteudoHTML = "";
+
+    tarefas.forEach((tarefa, indice) => {
+        let estiloConcluido = tarefa.concluida ? "text-decoration: line-through; opacity: 0.8;" : "color: #000000;";
+        
+        conteudoHTML += `
+            <tr style="${estiloConcluido}">
+                <td>${tarefa.nome}</td>
+                <td>Area</td>
+                <td>
+                    <button class="button_js1" onclick="alternarTarefa(${indice})">Concluir</button>
+                    <button class="button_js12" onclick="deletarTarefa(${indice})">Excluir</button>
+                </td>
+            </tr>
+        `;
+    });
+
+    div_tableMatResId.innerHTML = conteudoHTML;
+}
+
+function limparInputs(){
+
+    document.getElementById('input_textMatId').value = '';
+}
+
+function alternarTarefa(indice) {
+    tarefas[indice].concluida = !tarefas[indice].concluida;
+    window.confirm("Está tarefa foi concluida?");
+    mostrar();
+    
+}
+
+function deletarTarefa(indice) {
+    tarefas.splice(indice, 1);
+    window.alert("Tarefa Excluída!");
+    mostrar();
+}
