7ea35c4729480f1d8c90433fba2dfe0d7ee55e1e
mcabetee <marianacabete01@gmail>
2026-08-18 22:33:44 -0300

    versão 11

--- JAVASCRIPT/principal.js
+++ JAVASCRIPT/principal.js
@@ -1,3 +1,4 @@
+let materias = [];
 let tarefas = [];
 
 function cadastrarMat() {
@@ -7,14 +8,14 @@ function cadastrarMat() {
 
     if (input_textMatId !== '') {
 
-        tarefas.push({
+        materias.push({
 
             nome: input_textMatId,
             materia: select_materiaId
 
         });
 
-        mostrar();
+        mostrarMat();
 
     } else {
 
@@ -26,21 +27,21 @@ function cadastrarMat() {
 
 }
 
-function mostrar() {
+function mostrarMat() {
 
     let div_tableMatResId = document.getElementById('div_tableMatResId');
     let conteudoHTML = "";
 
-    tarefas.forEach((tarefa, indice) => {
-        let estiloConcluido = tarefa.concluida ? "text-decoration: line-through; opacity: 0.8;" : "color: #000000;";
+    materias.forEach((materia, indice) => {
+        let estiloConcluido = materia.concluida ? "text-decoration: line-through; opacity: 0.8;" : "color: #000000;";
 
         conteudoHTML += `
             <tr style="${estiloConcluido}">
-                <td>${tarefa.nome}</td>
-                <td>${tarefa.materia}</td>
+                <td>${materia.nome}</td>
+                <td>${materia.materia}</td>
                 <td>
-                    <button class="button_js1" onclick="alternarTarefa(${indice})">Concluir</button>
-                    <button class="button_js2" onclick="deletarTarefa(${indice})">Excluir</button>
+                    <button class="button_js1" onclick="alternarMateria(${indice})">Concluir</button>
+                    <button class="button_js2" onclick="deletarMateria(${indice})">Excluir</button>
                 </td>
             </tr>
         `;
@@ -49,20 +50,75 @@ function mostrar() {
     div_tableMatResId.innerHTML = conteudoHTML;
 }
 
-function alternarTarefa(indice) {
-    tarefas[indice].concluida = !tarefas[indice].concluida;
+function alternarMateria(indice) {
+    materias[indice].concluida = !materias[indice].concluida;
     window.confirm("Está tarefa foi concluida?");
-    mostrar();
+    mostrarMat();
 
 }
 
-function deletarTarefa(indice) {
-    tarefas.splice(indice, 1);
+function deletarMateria(indice) {
+    materias.splice(indice, 1);
     window.alert("Tarefa Excluída!");
-    mostrar();
+    mostrarMat();
+}
+
+
+function cadastrarTare(){
+
+    let input_textTareId = document.getElementById('input_textTareId').value
+    let select_tarefasId = document.getElementById('select_tarefasId').value
+
+    div_tableTarResId.innerHTML = '';
+
+    if(input_textTareId !== '' && select_tarefasId !== '' ){
+
+        tarefas.push({
+
+            nome:input_textTareId,
+            materia:select_tarefasId
+
+        });
+
+        mostrarTare()
+
+    } else {
+
+        alert('Preencha todos as campos')
+
+    }
+
+    limparInputs();
+
+}
+
+function mostrarTare(){
+    
+    let div_tableTarResId = document.getElementById('div_tableTarResId')
+
+    tarefas.forEach((tarefa, indice) => {
+
+        div_tableTarResId.innerHTML += `
+        
+        <td>${tarefa.nome}</td>
+        <td>${tarefa.materia}</td>
+        <td>Materia</td>
+        <td>area</td>
+        <td>expira</td>
+        <td>
+        <button>Concluirr</button>
+        <button>Excluir</button>
+        </td>
+
+        `
+        
+    });
+
 }
 
 function limparInputs() {
 
     document.getElementById('input_textMatId').value = '';
+    document.getElementById('input_textTareId').value = '';
+
 }
